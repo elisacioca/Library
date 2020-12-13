026c08a7ca4261722118468a35111b83622fe841
elison_stefani <elison.stefani@gmail.com>
2020-12-13 14:16:03 +0200

    Added models and needed consts

---
+++ LibraryAPI/LibraryAPI/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAPI.Models
+{
+    public class Author
+    {
+        public Guid AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfDeath { get; set; }   
+        public List<IPublication> Publications { get; set; }
+    }
+}
---
+++ LibraryAPI/LibraryAPI/Models/IPublication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAPI.Models
+{
+    public interface IPublication
+    {
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public DateTime PublicationDate { get; set; }
+        public List<string> Tags { get; set; }
+        public int NoOfCopies { get; set; }
+    }
+}
